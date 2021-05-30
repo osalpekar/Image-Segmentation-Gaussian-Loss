78620e0d2ba4a8fa3d62e2f10375c36fad4a205f
Omkar Salpekar <osalpekar@gmail.com>
2021-05-30 08:19:35 +0000

    modularized data creation code to extend siamese expreiment

---
+++ data.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import glob
+
+import torch
+import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+import sklearn.metrics
+from fastai.vision import *
+
+# Image File Paths
+IMG_DIR = "../data/images-256/*.png"
+MASK_DIR = "../data/masks-256/*.png"
+
+def get_data():
+    # Get Image and Mask Filenames
+    ims = glob.glob(IMG_DIR)
+    masks = glob.glob(MASK_DIR)
+
+    # Create pandas dataframe of filepaths. The validation column indicates
+    # whether the column is part of the validation set or not.
+    df = pd.DataFrame({
+        "img_path"   : ims,
+        "mask_path"  : masks,
+        "validation" : False,
+    })
+
+    # Add Scene ID and area
+    df['scene_id'] = df['img_path'].apply(lambda x: x.split("_")[1])
+    df['area'] = df['img_path'].apply(lambda x: x.split("_")[0].split("/")[-1])
+
+    # Let's use ptn as a valid set. Can also split randomly
+    df.loc[df.area == 'dar', 'validation'] = True
+    
+    return df
+
+class SiamDataset(Dataset):
+    def __init__(self, is_validation=False, num_train_examples=None):
+        df = get_data()
+        if num_train_examples is not None:
+            df = df.head(num_train_examples)
+        df = df[df['validation'] == is_validation]
+        self.df = df
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        row1 = self.df.iloc[idx]
+        row2 = self.df.iloc[(idx+1) % len(self.df)]
+
+        img1 = PIL.Image.open(row1.img_path)
+        img2 = PIL.Image.open(row2.img_path)
+        mask1 = PIL.Image.open(mask1.mask_path)
+        mask2 = PIL.Image.open(mask2.mask_path)
+
+        img1 = torch.from_numpy(img1).float()/255
+        img2 = torch.from_numpy(img2).float()/255
+        mask1 = torch.from_numpy(mask1).float()
+        mask2 = torch.from_numpy(mask2).float()
+        label = torch.randint(0,1,(1,)) # This would be label on whether they are smae or differet
+
+        return img1, img2, mask1, mask2, label
+
+def create_standard_databunch(batch_size, num_train_examples):
+    # Override open method of SegmentationLabelList since our masks are 0 for class 0, 255 for class 1 (so need div=True)
+    def my_open(self, fn): return open_mask(fn, div=True)
+    SegmentationLabelList.open = my_open
+
+    df = get_data()
+    print(df.head())
+    df_small = df.head(num_train_examples)
+    src = (SegmentationItemList.from_df(path='', df=df_small, cols='img_path')
+           .split_from_df(col='validation')
+           .label_from_df(cols='mask_path', classes=["building", "not"]))
+
+    data = (src.transform(get_transforms(), size=256, tfm_y=True)
+            .databunch(bs=batch_size) # Change batch size if you're having memory issues
+            .normalize(imagenet_stats))
+
+    return data
+
+def create_siamese_databunch(batch_size, num_train_examples):
+    train_ds = SiamDataset(False, num_train_examples)
+    valid_ds = SiamDataset(True, num_train_examples)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    valid_dl = DataLoader(valid_ds, batch_size=batch_size, shuffle=False)
+    data = DataBunch(train_dl, valid_dl)
+
+    return data
--- model.py
+++ model.py
@@ -12,7 +12,7 @@ from fastai.vision.models.unet import *
 
 from metrics import jaccard
 from loss import BootstrapStaticLoss, BootstrapDynamicLoss, DMILoss
-from data import get_data, SiamDataset
+from data import get_data, create_siamese_databunch, create_standard_databunch
 
 # Image File Paths
 IMG_DIR = "../data/images-256/*.png"
@@ -25,37 +25,19 @@ NUM_EPOCHS = 2
 LR = 1e-5 
 
 
-df = get_data()
-print("completed Data Parsing")
-print(df.head())
+#df = get_data()
+#print(df.head())
 
 # How many ims for each area?
 #df.groupby('area').count()['scene_id']
 
 
-df_small = df.head(NUM_TRAIN_EXAMPLES)
-"""
-# Override open method of SegmentationLabelList since our masks are 0 for class 0, 255 for class 1 (so need div=True)
-def my_open(self, fn): return open_mask(fn, div=True)
-SegmentationLabelList.open = my_open
+#df_small = df.head(NUM_TRAIN_EXAMPLES)
 
+data = create_standard_databunch(BATCH_SIZE, NUM_TRAIN_EXAMPLES)
+#data = create_siamese_databunch(BATCH_SIZE, NUM_TRAIN_EXAMPLES)
 
-np.random.seed(42)
-src = (SegmentationItemList.from_df(path='', df=df_small, cols='img_path')
-       .split_from_df(col='validation')
-       .label_from_df(cols='mask_path', classes=["building", "not"]))
-
-data = (src.transform(get_transforms(), size=256, tfm_y=True)
-        .databunch(bs=BATCH_SIZE) # Change batch size if you're having memory issues
-        .normalize(imagenet_stats))
-"""
-
-train_ds = SiamDataset(is_validation=False)
-valid_ds = SiamDataset(is_validation=True)
-train_dl = DataLoader(train_ds, batch_size=BATCH_SIZE, shuffle=True)
-valid_dl = DataLoader(valid_ds, batch_size=BATCH_SIZE, shuffle=False)
-data = DataBunch(train_dl, valid_dl)
-
+print("completed Data Parsing")
 # Check out a batch
 #data.show_batch(2, figsize=(10,7))
 print("Done assembling databunch")
@@ -85,6 +67,7 @@ class ContrastiveLoss(nn.Module):
         loss = 0.5* ((pdist**2) + (F.relu(self.margin-ndist)**2))
         return loss.mean() if size_average else losses.sum()
 
+"""
 model = SiameseNet().cuda()
 #apply_init(model.head, nn.init.kaiming_normal_)
 loss_func=ContrastiveLoss().cuda()
@@ -104,11 +87,10 @@ print("Constructed trainer")
 
 # Train!
 learn.fit_one_cycle(NUM_EPOCHS, slice(LR))
-"""
 
-src = (SegmentationItemList.from_df(path='', df=df_small[:4], cols='img_path')) # Did in two batches
+#src = (SegmentationItemList.from_df(path='', df=df_small[:4], cols='img_path')) # Did in two batches
 #learn = load_learner('') # Loads the exported learner from earlier
-learn.data.add_test(src, tfms=None, tfm_y=False)
-preds, y = learn.get_preds(DatasetType.Test) # Careful with ram - do in batches if needed ()
+#learn.data.add_test(src, tfms=None, tfm_y=False)
+#preds, y = learn.get_preds(DatasetType.Test) # Careful with ram - do in batches if needed ()
 #p,_,_ = learn.predict(df_small.head(1))
-print(preds)
+#print(preds)
